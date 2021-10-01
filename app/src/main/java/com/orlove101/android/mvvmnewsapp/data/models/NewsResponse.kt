4980238b04b1f598666ffa5df18cbf562893afba
OrLove101 <danyazerorebel@gmail.com>
2021-10-01 12:00:20 +0300

    fapp complited

--- app/src/main/java/com/orlove101/android/mvvmnewsapp/data/models/NewsResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.orlove101.android.mvvmnewsapp.data.models
-
-data class NewsResponse(
-    val articles: MutableList<Article>,
-    val status: String,
-    val totalResults: Int
-)
--- app/src/main/java/com/orlove101/android/mvvmnewsapp/data/models/Source.kt
+++
@@ -1,6 +0,0 @@
-package com.orlove101.android.mvvmnewsapp.data.models
-
-data class Source(
-    val id: Any,
-    val name: String
-)
